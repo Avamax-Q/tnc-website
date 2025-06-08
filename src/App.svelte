6fbad6283be477eb88f76eddaec4ddc404ea0ce2
Avamax-Q <avanish.gharat@gmail.com>
2025-06-08 18:42:57 +0530

    logo fix

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,7 @@
 
 <main>
   <div class="fixed-controls">
-    <img src="/static/tnc-logo.png" alt="TNC Logo" class="fixed-logo" />
+    <img src="/static/assets/tnc-logo.png" alt="TNC Logo" class="fixed-logo" />
     <button class="goto-top" on:click={scrollToTop}>↑</button>
   </div>
   <About />
